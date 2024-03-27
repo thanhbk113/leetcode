dfa9e88c8c1819fa85fe263acf9f5f0cd9c27593
thanhbk113 <thanhbk11a1@gmail.com>
2024-03-27 21:53:19 +0700

    Refactor trap function to calculate water trapped

--- hard/42.TrappingRainWater.go
+++ hard/42.TrappingRainWater.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func trap(h []int) int {
 	n := len(h)
 	waters := 0
@@ -20,9 +18,4 @@ func trap(h []int) int {
 	}
 
 	return waters
-}
-
-func main() {
-	heights := []int{1, 0, 1, 0, 2, 0, 1}
-	fmt.Println(trap(heights))
 }
